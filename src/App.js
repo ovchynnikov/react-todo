91890413c76c3efa84eacb8a64c2225e625401ba
ovchynnikov <vapore18@gmail.com>
2020-07-26 12:10:23 +0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import TodoList from './Components/TodoList';
+
 
 function App() {
+  const todos = [
+    { id: 1, completed: false, title: 'Buy milk'},
+    { id: 2, completed: false, title: 'Buy apples'},
+    { id: 3, completed: false, title: 'Buy 2 forks'}
+  ]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='wrapper'>
+      <h1>React tutorial</h1>
+      <TodoList todos={todos}/>
     </div>
+    
   );
 }
 
---
+++ src/Components/TodoList.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import TodoItem from './TodoItem';
+import PropTypes from 'prop-types';
+
+function TodoList(props){
+    return (
+        <ul>
+        { props.todos.map((todo, index )=> {
+            return <TodoItem todo={todo} key={todo.id} index={index} />
+        }) }
+        </ul>
+    )
+}
+
+TodoList.propTypes = {
+    todos: PropTypes.arrayOf(PropTypes.object).isRequired
+}
+
+export default TodoList;
